78e4a0f082e29bccf91f86b6676ca102bd0ebc0a
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-15 10:22:58 -0700

    adding initial js

---
+++ limbo/static/limbo/js/limbo.js
@@ -0,0 +1,16 @@
+/*!
+ * js for Limbo server
+ * Written by Keith McKinley, Fall 2016
+ * for CS419, Oregon State University
+ * 
+ */
+ 
+ document.getElementById("UsersEdit").onclick = function() { 
+	alert('UsersEdit'); 
+};
+ document.getElementById("EquipmentEdit").onclick = function() { 
+	alert('EquipmentEdit'); 
+};
+ document.getElementById("ServerEdit").onclick = function() { 
+	alert('ServerEdit'); 
+};
