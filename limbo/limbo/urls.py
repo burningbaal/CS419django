a2157924b83aa61e7edd50e92402a4bb24724c31
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-15 10:56:52 -0700

    still trying to get index.html working...

--- limbo/limbo/urls.py
+++ limbo/limbo/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.conf.urls.static import static
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-	url(r'^polls/', include('polls.urls')),
-	url(r'^admin/', admin.site.urls),
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	url(r'^index.html$', views.index, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
