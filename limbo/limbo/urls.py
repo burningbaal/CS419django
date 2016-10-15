f534f453cc6bfca2c611aca62072ea4eed28c146
Keith <mckinleykj@gmail.com>
2016-10-15 22:20:36 +0000

    Fixed so no index.html is needed, but accepted

--- limbo/limbo/urls.py
+++ limbo/limbo/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-	url(r'^index$', views.index, name='index'),
+	url(r'^(?:index\.?[html]{,4})?$', views.index, name='index'),
+#	url(r'^index$', views.index, name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
