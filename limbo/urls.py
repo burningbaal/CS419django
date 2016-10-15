36586ed8b38b85f137eadf4977e58e6fdbe4fa6c
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-15 12:09:24 -0700

    reverting these files to 6bbeaca4765725a5e42fbd9caf4caf44de5c3a49

--- limbo/urls.py
+++ limbo/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^index$', include('limbo.urls')),
-	url(r'^limbo/', include('limbo.urls')),
 	url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', admin.site.urls),
+	url(r'^limbo/', include('limbo.limbo.urls')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
