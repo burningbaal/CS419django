87c967c8e1d628f40f6d7922778009cbb50dc4c1
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-15 10:38:06 -0700

    still trying to serve index.html

--- limbo/urls.py
+++ limbo/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', admin.site.urls),
-	url(r'^index.html', include('polls.urls'))
+	url(r'^$', include('limbo.urls'))
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
