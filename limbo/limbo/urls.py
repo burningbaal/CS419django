897817689db63b55100192ed3e2c2ad1753417f1
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-15 11:17:09 -0700

    still trying to get index to work

--- limbo/limbo/urls.py
+++ limbo/limbo/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^index$', views.index, name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- limbo/urls.py
+++ limbo/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^$', include('limbo.urls')),
+	url(r'^index$', include('limbo.urls')),
 	url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', admin.site.urls),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
