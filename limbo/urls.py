4d0721a8cac0e18cd459f570dd6d5156a75d19b1
Keith <mckinleykj@gmail.com>
2016-10-15 18:49:41 +0000

    Revert "still trying to make index work"
    This reverts commit 046a90ddf53c683f2fa10bf28f644c8d311df6c7.

--- limbo/urls.py
+++ limbo/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^index$', include('limbo.urls')),
-	url(r'^limbo/', include('limbo.urls')),
 	url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', admin.site.urls),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
