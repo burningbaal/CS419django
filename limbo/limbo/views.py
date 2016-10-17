990e8a9e19ad16b51aae4ed9440458ce168a68e0
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-16 18:42:40 -0700

    cleaned up users and editUsers views, drafted equipment and editEquipment views

--- limbo/limbo/views.py
+++ limbo/limbo/views.py
@@ -22,48 +22,48 @@ def editUsers(request):
 		# redirect to a new URL:
 		if form.is_valid():
 			name = form.cleaned_data['user_name']
-			# name = json.dumps(form.data['user_name'])
 			request.session['editUserName'] = name
 			
-			# call out to limboLogic.py to update values
+			# call out to limboLogic.py to update values, add them to the session
 			
-			test = name
 			return redirect('../users')
 	# if a GET (or any other method) we'll create a blank form
+	request.session.flush()
 	return redirect('../users')
 
 from .forms import *
 
 def users(request):
 	form = None
-	
 	if 'editUserName' not in request.session:
 		# create a blank form
 		form = usersForm()
 	else:
-		# form = equipmentForm(initial='jim') - used to make sure I was branching the if/else correctly
 		form = usersForm(initial={'user_name':request.session['editUserName']}, auto_id=False) #limboLogic.GetUserInfo(name))
 	return render(request, 'limboHtml/UserManagement.html', {'form': form})
 		
 def editEquipment(request):
-	return HttpHttpResponse("This capability isn't built yet, please go back")
+	if request.method == 'POST':
+		form = equipmentForm(request.POST)
+		if form.is_valid():
+			manuf_email = form.cleaned_data['manuf_email']
+			request.session['editEquipId'] = manuf_email
+			
+			# call out to limboLogic.py to update values, add them to the session
+			
+			return redirect('../users')
+	# if a GET (or any other method) we'll create a blank form
+	return redirect('../users')
 
 def equipment(request):
-        # if this is a POST request we need to process the form data
-        if request.method == 'POST':
-                # create a form instance and populate it with data from the request:
-                form = equipmentForm(request.POST)
-                # check whether it's valid:
-                # process the data in form.cleaned_data as required
-                # redirect to a new URL:
-                name = json.dumps(form.data['manuf_email'])
-                request.session['name'] = name
-                return redirect('/equipment' )
-
-        # if a GET (or any other method) we'll create a blank form
-        else:
-                form = equipmentForm()
-        return render(request, 'limboHtml/EquipmentManagement.html', {'form': form})
+	form = None
+	if 'editUserName' not in request.session:
+		# create a blank form
+		form = equipmentForm()
+	else:
+		form = equipmentForm(initial={'manuf_email':request.session['editEquipId']}, auto_id=False) #limboLogic.GetUserInfo(name))
+	return render(request, 'limboHtml/EquipmentManagement.html', {'form': form})
+	
 		
 def editServer(request):
 	return HttpHttpResponse("This capability isn't built yet, please go back")
