543adac35ec9f5128aae7671ef6264ea97d6ba87
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-29 15:16:30 -0700

    trying .disabled flag on keys

--- limbo/limbo/models.py
+++ limbo/limbo/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class serverConfig(models.Model):
 	
 	config_key = models.CharField(max_length=63)
+	config_key.disabled
 	config_value = models.CharField(max_length=63)
