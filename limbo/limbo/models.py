fd16e772317ae4efbcf50710d55c02af3a15671a
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-29 15:23:30 -0700

    still trying to get key disabled

--- limbo/limbo/models.py
+++ limbo/limbo/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class serverConfig(models.Model):
 	
-	config_key = models.CharField(widget=self.TextInput(attrs={'readonly':'readonly'}))
+	config_key = models.CharField(max_length=63, attrs={'readonly':'readonly'})
 	config_value = models.CharField(max_length=63)
