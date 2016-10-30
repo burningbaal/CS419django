fe4ec8fab45762472ca88f317a90f974947c89f4
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 15:19:46 -0700

    adding imports

--- limbo/urls.py
+++ limbo/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
 urlpatterns = [
 	url(r'^api/', include('api.urls')),
