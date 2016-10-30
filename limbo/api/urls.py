039c384987ec338e10d0eed1c4138651b969bb81
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 14:14:45 -0700

    first draft of api for usageHistory

--- limbo/api/urls.py
+++ limbo/api/urls.py
@@ -4,11 +4,5 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-	url(r'^(edit/)*(?:index\.?[html]{,4})?$', views.indexLimbo, name='indexLimbo'),
-	url(r'^(?:users\.?[html]{,4})?$', views.editServer, name='editServer'),
-	url(r'^(?:equipment\.?[html]{,4})?$', views.editEquipment, name='editEquipment'),
-	url(r'^(?:server\.?[html]{,4})?$', views.editServer, name='editServer'),
-	url(r'^edit/users.html$', views.editUsers, name='editUsers'),
-	url(r'^edit/equipment.html$', views.editEquipment, name='editEqiupment'),
-	url(r'^edit/server.html$', views.editServer, name='editServer'),
+	url(r'^(?:addUse\.?[html]{,4})?$', views.addUsageHistory, name='addUsageHistory'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- limbo/api/views.py
+++ limbo/api/views.py
@@ -13,31 +13,30 @@ from limbo.models import *
 from django.forms import modelformset_factory
 from django.forms import formset_factory
 
-def editServer(request):
-	result = serverConfig.objects.values()
+def addUsageHistory(request):
+	result = usageHistory.objects.values()
 	myConfigs = [entry for entry in result]
 	
-	finalFormSet = modelformset_factory(serverConfig, exclude=('id',), extra=0)
+	finalFormSet = modelformset_factory(usageHistory, exclude=('none'), extra=0) # intentionally not a tuple
 	if request.method == 'POST':
 		formset = finalFormSet(request.POST, request.FILES)
 		if formset.is_valid():
 			for form in formset:
 				form.save()
 			
-			# call out to limboLogic.py to update values, add them to the session
-			message = 'The values have been updated.'
-			return render(request, 'limboHtml/ServerConfiguration.html', {'formset': finalFormSet, 'SubmitMessage': '', 'CurrentConfigs': myConfigs})
+			message = HttpResponse(serializers.serialize("json", usageHistory.objects.filter(pk=usageHistory.Id)))
+			HttpResponse(message)
+			#return render(request, 'limboHtml/ServerConfiguration.html', {'formset': finalFormSet, 'SubmitMessage': '', 'CurrentConfigs': myConfigs})
 		else:
-			message = 'The server configuration has NOT been updated.' + '\n'
-			message += ', '.join("%s=%r" % (key,val) for (key,val) in form.errors.iteritems()) + '\n' 
-			message += '<br> ' + ", ".join("%s=%r" % (key,val) for(key,val) in form.iteritems()) 
-			# message += ', '.join("%s=%r" % (key,val) for (key,val) in form.non_field_errors.iteritems()) + '\n' 
-			return render(request, 'limboHtml/ServerConfiguration.html', {'form': form, 'SubmitMessage': message, 'CurrentConfigs': myConfigs})
+			message = '{"Error": ["Message":"The server configuration has NOT been updated.",' + '\n'
+			message += '"Details":"' + ', '.join("%s=%r" % (key,val) for (key,val) in form.errors.iteritems()) + '"]}' 
+			#message += '<br> ' + ", ".join("%s=%r" % (key,val) for(key,val) in form.iteritems()) 
+			HttpResponse(message)
 	# if a GET (or any other method) we'll create a blank form
 	try:
 		del request.session['integer']
 	except KeyError:
 		pass
-	
-	return render(request, 'limboHtml/ServerConfiguration.html', {'formset': finalFormSet, 'SubmitMessage': '', 'CurrentConfigs': myConfigs})
+	message = '{"Error":"Data must be POSTed"}'
+	return HttpResponse(message)
 
