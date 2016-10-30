fbf6388e21256db75ddac74ef2668e40d973ff00
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 14:22:02 -0700

    importing apps

--- limbo/limbo/urls.py
+++ limbo/limbo/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
+from django.contrib import admin
+from . import api, polls
 from . import views
 
 urlpatterns = [
