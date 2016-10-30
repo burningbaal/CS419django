710730dffc3ff6f47b1f57d430e8d584b8bed6ac
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 14:20:49 -0700

    trying to trigger polls.urls.py

--- limbo/limbo/urls.py
+++ limbo/limbo/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+	url(r'^api/', include('api.urls')),
+	url(r'^polls/', include('polls.urls')),
+	url(r'^admin/', admin.site.urls),
 	url(r'^(edit/)*(?:index\.?[html]{,4})?$', views.indexLimbo, name='indexLimbo'),
 	url(r'^edit/users.html$', views.editUsers, name='editUsers'),
 	url(r'^edit/equipment.html$', views.editEquipment, name='editEqiupment'),
