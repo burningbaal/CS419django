bd999877f97ff89dc7456407e1a6af9dd24e5447
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 14:16:11 -0700

    moving api to top of urls.py

--- limbo/urls.py
+++ limbo/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^index$', include('limbo.urls')),
 	url(r'^api/', include('api.urls')),
+	url(r'^index$', include('limbo.urls')),
 	url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', admin.site.urls),
 	url(r'^limbo/', include('limbo.limbo.urls')),
