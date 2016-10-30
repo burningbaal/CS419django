e067c5f69ba4853953dc15324e0d10ec3ad04513
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 15:11:53 -0700

    fixing caps

--- limbo/api/models.py
+++ limbo/api/models.py
@@ -6,4 +6,4 @@ class usageHistory(models.Model):
 	fk_employee_number = models.BigIntegerField()
 	fk_version = models.BigIntegerField()
 	fk_instrument = models.BigIntegerField()
-	timestamp = models.dateTimeField(auto_now=False, auto_now_add=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
