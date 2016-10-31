663296df117ecce0ad6067ddac9999ce12c48870
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-10-30 20:30:58 -0700

    trying the idea on  http://stackoverflow.com/questions/30871033/django-rest-framework-remove-csrf to address the csrf error

--- limbo/api/views.py
+++ limbo/api/views.py
@@ -11,8 +11,15 @@ from forms import *
 from models import *
 from django.forms import modelformset_factory
 from django.forms import formset_factory
+from rest_framework.authentication import SessionAuthentication 
 
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+    def enforce_csrf(self, request):
+        return  # To not perform the csrf check previously happening
+authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 def addUsageHistory(request):
+	authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 	result = usageHistory.objects.values()
 	myConfigs = [entry for entry in result]
 	
