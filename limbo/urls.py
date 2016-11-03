c2debbfd5807925f80b78f4268c9511a2f12e4d4
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-02 19:58:03 -0700

    commenting out old urls to simplify

--- limbo/urls.py
+++ limbo/urls.py
@@ -7,9 +7,9 @@ from django.conf import settings
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^api/', include('api.urls')),
-	url(r'^index', include('limbo.urls')),
-	url(r'^polls/', include('polls.urls')),
-	url(r'^limbo/', include('limbo.urls')),
+	# url(r'^index', include('limbo.urls')),
+	#url(r'^polls/', include('polls.urls')),
+	# url(r'^limbo/', include('limbo.urls')),
 	url(r'edit/', include('limbo.urls')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
