6fc0557f161eecad490d5cc44f257a1e925627c4
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-02 20:11:57 -0700

    re-adding api app

--- limbo/urls.py
+++ limbo/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	# url(r'^api/', include('api.urls')),
+	url(r'^api/', include('api.urls')),
 	#url(r'^index', include('limbo.urls')),
 	#url(r'^polls/', include('polls.urls')),
 	# url(r'^limbo/', include('limbo.urls')),
