d689423614715a1d339cc46b2ea3f3212486a753
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 18:49:21 -0700

    switching to ref local UserProfile class instead

--- limbo/limbo/models.py
+++ limbo/limbo/models.py
@@ -72,7 +72,7 @@ class Instrument(models.Model):
 class Instr_Version(models.Model):
 	FK_version = models.ForeignKey(Version, on_delete=models.CASCADE)
 	FK_instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
-	validating_user = models.ForeignKey(auth.user, on_delete=models.models.PROTECT)
+	validating_user = models.ForeignKey(UserProfile, on_delete=models.models.PROTECT)
 	timestamp = models.DateField(auto_now_add=True)
 
 	class Meta:
@@ -80,17 +80,17 @@ class Instr_Version(models.Model):
 
 class User_Version(models.Model):
 	FK_version = models.ForeignKey(Version, on_delete=models.CASCADE)
-	FK_user = models.ForeignKey(auth.user, on_delete=models.CASCADE)
-	validating_user = models.ForeignKey(auth.user, on_delete=models.PROTECT)
+	FK_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	validating_user = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
 	timestamp = models.DateField(auto_now_add=True)
 
 	class Meta:
 		unique_together = ('FK_version', 'FK_user')
 
 # class User_Permission(models.Model):
-	# FK_user = models.ForeignKey(auth.user, on_delete=models.CASCADE)
+	# FK_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 	# FK_permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
-	# validating_user = models.ForeignKey(auth.user, on_delete=models.PROTECT)
+	# validating_user = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
 	# timestamp = models.DateField(auto_now_add=True)
 	
 	# class Meta:
@@ -104,7 +104,7 @@ class User_Version(models.Model):
 		# unique_together = ('FK_role', 'FK_permission')
 
 class UsageHistory(models.Model):
-	FK_user = models.ForeignKey(auth.user, on_delete=models.PROTECT)
+	FK_user = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
 	FK_version = models.ForeignKey(Version, on_delete=models.PROTECT)
 	FK_instrument = models.ForeignKey(Instrument, on_delete=models.PROTECT)
 	timestamp = models.DateField(auto_now_add=True)
