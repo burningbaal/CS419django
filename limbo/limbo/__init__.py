346e41ed26cf32356a318b0579f298b34c95a87f
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-04 15:37:43 -0700

    fixing the comment-out

--- limbo/limbo/__init__.py
+++ limbo/limbo/__init__.py
@@ -9,14 +9,14 @@
     # This syncdb hooks takes care of adding a view permission too all our 
     # content types.
     # """
-    for each of our content types
+    ##for each of our content types
     # for content_type in ContentType.objects.all():
-        build our permission slug
+        ##build our permission slug
         # codename = "view_%s" % content_type.model
 
-        if it doesn't exist..
+        #if it doesn't exist..
         # if not Permission.objects.filter(content_type=content_type, codename=codename):
-            add it
+            ##add it
             # Permission.objects.create(content_type=content_type,
                                       # codename=codename,
                                       # name="Can view %s" % content_type.name)
