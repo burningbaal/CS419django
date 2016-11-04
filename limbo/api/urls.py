e16ef1bd5abb6c9ec205c5bc707713205d7d893a
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 19:31:30 -0700

    fixing name conflict on imported classes

--- limbo/api/urls.py
+++ limbo/api/urls.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from . import views
-from rest_framework.authtoken import views
+from rest_framework.authtoken import restFwViews
 
 urlpatterns = [
 	url(r'^addUse/', views.addUsageHistory, name='addUsageHistory'),
-	url(r'^api-token-auth/', views.obtain_auth_token),
+	url(r'^api-token-auth/', restFwViews.obtain_auth_token),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
