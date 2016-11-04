02092bb7a9f6b3d1b29c877e61a9fa1dc1b9b6f4
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 19:29:50 -0700

    adding ref to from rest_framework.authtoken import views

--- limbo/api/urls.py
+++ limbo/api/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from . import views
+from rest_framework.authtoken import views
 
 urlpatterns = [
 	url(r'^addUse.html$', views.addUsageHistory, name='addUsageHistory'),
