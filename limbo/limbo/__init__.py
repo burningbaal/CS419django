6c03152a431e647fe4f55eba204c9fad23870586
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-04 15:36:57 -0700

    disabling the add_view code for now

--- limbo/limbo/__init__.py
+++ limbo/limbo/__init__.py
@@ -1,26 +1,26 @@
 # from: http://blog.nyaruka.com/adding-a-view-permission-to-django-models
 # on 04Nov2016
-from django.db.models.signals import post_migrate # post_syncdb replaced at v1.9
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import Permission
+# from django.db.models.signals import post_migrate # post_syncdb replaced at v1.9
+# from django.contrib.contenttypes.models import ContentType
+# from django.contrib.auth.models import Permission
 
-def add_view_permissions(sender, **kwargs):
-    """
-    This syncdb hooks takes care of adding a view permission too all our 
-    content types.
-    """
-    # for each of our content types
-    for content_type in ContentType.objects.all():
-        # build our permission slug
-        codename = "view_%s" % content_type.model
+# def add_view_permissions(sender, **kwargs):
+    # """
+    # This syncdb hooks takes care of adding a view permission too all our 
+    # content types.
+    # """
+    for each of our content types
+    # for content_type in ContentType.objects.all():
+        build our permission slug
+        # codename = "view_%s" % content_type.model
 
-        # if it doesn't exist..
-        if not Permission.objects.filter(content_type=content_type, codename=codename):
-            # add it
-            Permission.objects.create(content_type=content_type,
-                                      codename=codename,
-                                      name="Can view %s" % content_type.name)
-            print "Added view permission for %s" % content_type.name
+        if it doesn't exist..
+        # if not Permission.objects.filter(content_type=content_type, codename=codename):
+            add it
+            # Permission.objects.create(content_type=content_type,
+                                      # codename=codename,
+                                      # name="Can view %s" % content_type.name)
+            # print "Added view permission for %s" % content_type.name
 
 # check for all our view permissions after a syncdb
 #post_syncdb.connect(add_view_permissions)
