818b711c6a18a3c2c515a6a56a11e619058b7580
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 21:21:48 -0700

    fixing ref of @APIView to @api_view

--- limbo/api/views.py
+++ limbo/api/views.py
@@ -14,9 +14,10 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication 
+from rest_framework.decorators import api_view
 
 @csrf_exempt
-@APIView(['GET', 'POST'])
+@api_view(['GET', 'POST'])
 def addUsageHistory(request):
 	# authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 	result = usageHistory.objects.values()
