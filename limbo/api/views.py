067153fbe6260beb6690e7c5e28f87a9508ffe20
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 21:14:04 -0700

    trying to add some includes

--- limbo/api/views.py
+++ limbo/api/views.py
@@ -13,8 +13,11 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication 
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 @csrf_exempt
+@APIView(['POST'])
 def addUsageHistory(request):
 	# authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 	result = usageHistory.objects.values()
