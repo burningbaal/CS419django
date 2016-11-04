d958c81fdb5e894d6650559e364f4707bdc30119
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-04 15:36:13 -0700

    temporarily disabling call to add_view

--- limbo/limbo/__init__.py
+++ limbo/limbo/__init__.py
@@ -23,4 +23,4 @@ def add_view_permissions(sender, **kwargs):
             print "Added view permission for %s" % content_type.name
 
 # check for all our view permissions after a syncdb
-post_syncdb.connect(add_view_permissions)
+#post_syncdb.connect(add_view_permissions)
