b8ef982d616fa58042768bb495943ccfe6751ab3
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 19:22:25 -0700

    starting to include djangorestframework auth classes

--- limbo/api/models.py
+++ limbo/api/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from datetime import datetime    
+from django.conf import settings
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 class usageHistory(models.Model):
 	
 	fk_employee_number = models.BigIntegerField()
 	fk_version = models.BigIntegerField()
 	fk_instrument = models.BigIntegerField()
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+	
+# remove this after first compile, it's just to create the already-existing set of users' tokens
+
+for user in User.objects.all():
+    Token.objects.get_or_create(user=user)
