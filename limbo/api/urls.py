7816fc7c65d46a5bdb785d4ca9baebf73a69cfdd
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-03 19:32:13 -0700

    bugfix

--- limbo/api/urls.py
+++ limbo/api/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from . import views
-from rest_framework.authtoken import restFwViews
+from rest_framework.authtoken import views as restFwViews
 
 urlpatterns = [
 	url(r'^addUse/', views.addUsageHistory, name='addUsageHistory'),
