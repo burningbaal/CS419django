5de0eeb4fef95988ae7aa2f30c0753a755a94dd7
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-04 12:09:43 -0700

    trying to reference the version number for the message

--- limbo/api/views.py
+++ limbo/api/views.py
@@ -33,7 +33,7 @@ def addUsageHistory(request):
 			#time = form.cleaned_data['timestamp']
 			model = UsageHistory(FK_user=FK_usr, FK_version=FK_ver, FK_instrument=FK_instr)
 			model.save()
-			message = HttpResponse('Added use with user: "' + str(FK_usr) + '", version: "' + str(FK_ver) + '", and instrument:' + str(FK_instr) + '".')
+			message = HttpResponse('Added use with user: "' + str(FK_usr) + '", version: "' + str(FK_ver.version_number) + '", and instrument:' + str(FK_instr) + '".')
 			return HttpResponse(message)
 		else:
 			message = '{"Error": ["Message":"The server configuration has NOT been updated.",' + '\n'
