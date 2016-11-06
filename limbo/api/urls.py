4cec1157419644d8bbd0232f3434ea0a0ad2cf32
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-06 15:15:04 -0800

    fixing typo in urls.py

--- limbo/api/urls.py
+++ limbo/api/urls.py
@@ -7,6 +7,6 @@ from rest_framework.authtoken import views as restFwViews
 urlpatterns = [
 	url(r'^addUse/', views.addUsageHistory, name='addUsageHistory'),
 	url(r'^getUse/', views.getUsageHistory, name='getUsageHistory'),
-	url(r'^getInstrument /', views.getInstrument, name='getInstrument'),
+	url(r'^getInstrument/', views.getInstrument, name='getInstrument'),
 	url(r'^api-token-auth/', restFwViews.obtain_auth_token),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
