cc036de2d982a0044b31a395c7af7880659f2118
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-09 21:00:52 -0800

    trying getUserModel as a function

--- limbo/limbo/serializers.py
+++ limbo/limbo/serializers.py
@@ -9,7 +9,7 @@ class serverConfigSerializer(serializers.ModelSerializer):
 	
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
-		model = settings.get_user_model 
+		model = settings.get_user_model()
 		fields = '__all__'
 		#excludes = ('password',)
 		
