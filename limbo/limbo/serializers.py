968520080b5cc67ac0d03f6d82615518ff72ce83
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-09 21:10:21 -0800

    troubleshooting

--- limbo/limbo/serializers.py
+++ limbo/limbo/serializers.py
@@ -15,7 +15,6 @@ class serverConfigSerializer(serializers.ModelSerializer):
 		
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.serialize(settings.AUTH_USER_MODEL, read_only=True)
-	#user = UserSerializer(read_only=True)
 	class Meta:
 		model = UserProfile
 		#fields = ('last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined')
