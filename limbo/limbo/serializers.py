79a2d6d11e22c44ea47414668e6086d29153ae36
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-09 21:06:37 -0800

    simplifying

--- limbo/limbo/serializers.py
+++ limbo/limbo/serializers.py
@@ -7,15 +7,15 @@ class serverConfigSerializer(serializers.ModelSerializer):
 		model = serverConfig
 		fields = '__all__'
 	
-class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = django.contrib.auth.get_user_model()
-		fields = '__all__'
+#class UserSerializer(serializers.ModelSerializer):
+	#class Meta:
+		#model = django.contrib.auth.get_user_model()
+		#fields = '__all__'
 		#excludes = ('password',)
 		
 class UserProfileSerializer(serializers.ModelSerializer):
     #user = serializers.serialize(settings.AUTH_USER_MODEL, read_only=True)
-	user = UserSerializer(read_only=True)
+	#user = UserSerializer(read_only=True)
 	class Meta:
 		model = UserProfile
 		#fields = ('last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined')
