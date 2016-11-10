a20e26769d8cbb5f71d237aeaddc6d9a75cac834
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-09 20:32:54 -0800

    seeing if HyperlinkedModelSerializer works better

--- limbo/limbo/serializers.py
+++ limbo/limbo/serializers.py
@@ -2,17 +2,17 @@ from rest_framework import serializers
 from django.conf import settings
 from limbo.models import *
 
-class serverConfigSerializer(serializers.ModelSerializer):
+class serverConfigSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = serverConfig
 		fields = '__all__'
 	
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = settings.AUTH_USER_MODEL
 		excludes = 'password'
 		
-class UserProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     #user = serializers.serialize(settings.AUTH_USER_MODEL, read_only=True)
 	user = UserSerializer(read_only=True)
 	class Meta:
@@ -21,18 +21,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
 		fields = '__all__'
 		#excludes = ('password',)
 	
-class InstrTypeSerializer(serializers.ModelSerializer):
+class InstrTypeSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = InstrType
 		fields = '__all__'
 
-class MethodSerializer(serializers.ModelSerializer):
+class MethodSerializer(serializers.HyperlinkedModelSerializer):
 	description = serializers.ReadOnlyField(source='descripton')
 	class Meta:
 		model = Method
 		fields = ('id', 'name', 'description')
 
-class VersionSerializer(serializers.ModelSerializer):
+class VersionSerializer(serializers.HyperlinkedModelSerializer):
 	method = MethodSerializer(read_only=True)
 	#Instr_Version = Instr_VersionSerializer(source='Instr_Version_set', many=True, read_only=True)
 	class Meta:
@@ -41,20 +41,20 @@ class VersionSerializer(serializers.ModelSerializer):
 
 class Instr_to_VersionSerializer(serializers.HyperlinkedModelSerializer):
 	version = VersionSerializer(source='FK_version', read_only=True, many=True)
-	validator = UserProfileSerializer(source='validating_user.user', read_only=True)
+	validator = UserProfileSerializer(source='validating_user', read_only=True)
 	#validator = serializers.ReadOnlyField(source='self.validating_user.user.email')
 	version_name = serializers.ReadOnlyField(source='version_number')
 	cmd_line_script = serializers.ReadOnlyField()
 	SOP = serializers.ReadOnlyField()
 	method = MethodSerializer(source='FK_method',read_only=True)
-	Time_Validated = serializers.ReadOnlyField(source= 'timestamp')
+	#Time_Validated = serializers.ReadOnlyField(source= 'timestamp')
 	
 	class Meta:
 		model = Instr_Version
-		fields = ('id', 'version', 'validator', 'Time_Validated', 'method', 'version_name', 'cmd_line_script', 'SOP')
+		fields = ('id', 'timestamp', 'version', 'validator', 'Time_Validated', 'method', 'version_name', 'cmd_line_script', 'SOP')
 
 
-class InstrumentSerializer(serializers.ModelSerializer):
+class InstrumentSerializer(serializers.HyperlinkedModelSerializer):
 	instr_type = InstrTypeSerializer(source='FK_instr_type', read_only=True)
 	Validated_Versions = Instr_to_VersionSerializer(source='VersionsFromInstrument', many=True, read_only=True)
 	
