de9316045a24b23ffcf904af4d0521688420a1bb
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-12 15:05:51 -0800

    adding check for empty forms

--- limbo/limbo/views.py
+++ limbo/limbo/views.py
@@ -55,6 +55,7 @@ def editEquipment(request):
 		postFormset = formSet(request.POST, request.FILES)
 		if postFormset.is_valid():
 			for form in postFormset:
+				if form.is_valid() and not form.empty_permitted
 				form.save()
 			# call out to limboLogic.py to update values, add them to the session
 			message = 'The values have been updated.'
