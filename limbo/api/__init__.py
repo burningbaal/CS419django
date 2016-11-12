c45340f4735a763d8958fb71d6a7a2a3ec6a3d54
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-12 11:45:31 -0800

    fixing getUser

--- limbo/api/__init__.py
+++ limbo/api/__init__.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 
-def getUser(username, password)
+def getUser(username, password):
 	user = None
 	user = authenticate(username=username, password=password)
 	return user
