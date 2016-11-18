0e428dd3234549e540abe565fe4fdacf59782e8b
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-17 20:40:54 -0800

    removed extra ),

--- limbo/limbo/forms.py
+++ limbo/limbo/forms.py
@@ -44,9 +44,8 @@ class MethodFormSetHelper(FormHelper):
 							css_class='btn', 
 							onclick="location.href='method?Method=" + 'id' + "';"
 						)
-					)
-					, css_class='col-md-4'
-				),
+					), 
+					css_class='col-md-4'
 				),
 			),
 		)
