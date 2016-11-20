4041382102bbed0d6bd122a8d293925f4d6d2702
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-20 11:37:21 -0800

    removing bad form

--- limbo/limbo/forms.py
+++ limbo/limbo/forms.py
@@ -112,16 +112,16 @@ class MethodVersionFormSetHelper(FormHelper):
         )
         self.render_required_fields = True
 
-class usersForm(ModelForm):
-	class Meta:
-		model = UserProfile.user
-		fields = (
-			'first_name', 
-			'last_name',
-			'user_permissions',
-			'groups',
-		)
-		widgets = {'id': forms.HiddenInput()}
+#class usersForm(ModelForm):
+	#class Meta:
+		#model = UserProfile.user
+		#fields = (
+			#'first_name', 
+			#'last_name',
+			#'user_permissions',
+			#'groups',
+		#)
+		#widgets = {'id': forms.HiddenInput()}
 		
 class usersFormSetHelper(FormHelper):
 	def __init__(self, *args, **kwargs):
