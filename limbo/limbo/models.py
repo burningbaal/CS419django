a9f019a30229ecad304ff5dcc964ff2a7e7cf9b7
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-22 19:54:41 -0800

    importing post_save and receiver

--- limbo/limbo/models.py
+++ limbo/limbo/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from datetime import datetime 
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class serverConfig(models.Model):
 	
