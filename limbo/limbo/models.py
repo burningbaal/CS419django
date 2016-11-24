2abd8614e32c2e6750bd1ab4b77616bc27cf70bb
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-23 19:25:44 -0800

    updating indentation

--- limbo/limbo/models.py
+++ limbo/limbo/models.py
@@ -22,16 +22,16 @@ class UserProfile(models.Model):
 	
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+	if created:
+		UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    try:
+	try:
 		instance.UserProfile.save()
 	except:
 		UserProfile.objects.create(user=instance)
-	
+
 class InstrType(models.Model):
 	def __str__(self):
 		return self.make + ' ' + self.model
