6adc7bf30035deef3f340f9d42431d6650e850e4
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-24 11:25:40 -0800

    adding related_names for Instr_Version model

--- limbo/limbo/models.py
+++ limbo/limbo/models.py
@@ -93,9 +93,9 @@ class Instrument(models.Model):
 		)
 
 class Instr_Version(models.Model):
-	FK_version = models.ForeignKey(Version, related_name='versions', on_delete=models.CASCADE)
+	FK_version = models.ForeignKey(Version, related_name='authorized_verions', on_delete=models.CASCADE)
 	FK_instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
-	validating_user = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+	validating_user = models.ForeignKey(UserProfile, related_name='user_versions_granted', on_delete=models.PROTECT)
 	timestamp = models.DateField(auto_now_add=True)
 
 	class Meta:
@@ -107,7 +107,7 @@ class Instr_Version(models.Model):
 class User_Version(models.Model):
 	FK_version = models.ForeignKey(Version, on_delete=models.CASCADE)
 	FK_user = models.ForeignKey(User, on_delete=models.CASCADE)
-	authorizing_user = models.ForeignKey(User, related_name='+', on_delete=models.PROTECT)
+	authorizing_user = models.ForeignKey(User, on_delete=models.PROTECT)
 	timestamp = models.DateField(auto_now_add=True)
 
 	class Meta:
