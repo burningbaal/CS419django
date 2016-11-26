48c10d1e48d15579c5dc83fad998629087c4e776
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-26 10:18:39 -0800

    import redirect

--- limbo/urls.py
+++ limbo/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 
 urlpatterns = [
