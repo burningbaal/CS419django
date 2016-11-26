5a00a4fc0b826f70942974267f84fe0e603fbe18
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-25 22:09:25 -0800

    trying to clean up search fields

--- limbo/limbo/admin.py
+++ limbo/limbo/admin.py
@@ -29,7 +29,7 @@ class UserProfileVersionInline(admin.StackedInline):
 class ProfileAdmin(admin.ModelAdmin):
 	inlines = [UserProfileVersionInline,]
 	list_display  = ('Last_name', 'First_name', 'title', 'user_link')
-	search_fields = ('user__Last_name', 'user__First_name', 'title',)
+	search_fields = ('user__last_name', 'user__first_name', 'title',)
 		
 	def Last_name(self, obj):
 		return obj.user.last_name
@@ -52,7 +52,7 @@ class CustomUserAdmin(UserAdmin):
 class UsageHistoryAdmin(admin.ModelAdmin):
 	verbose_name_plural = 'Usage History Logs'
 	list_display = ('User', 'Instrument','Method', 'Time',)
-	search_fields = ('User__FK_user', 'Instrument__instrument','version__FK_version',)
+	search_fields = ('FK_user__username', 'FK_user_lastname', 'FK_user__first_name', 'FK_instrument__name','FK_instrument__asset_number','FK_version__version_number',)
 	
 	def Time(self, obj):
 		return obj.timestamp
@@ -85,7 +85,7 @@ class Instr_VersionInline(admin.TabularInline):
 class InstrumentAdmin(admin.ModelAdmin):
 	inlines = [Instr_VersionInline,]
 	list_display  = ('name', 'asset_number', 'Instrument_type', 'serial_number', )
-	search_fields = ('name', 'asset_number', 'InstrType__FK_instr_type', 'serial_number', )
+	search_fields = ('name', 'asset_number', 'FK_instr_type__InstrType', 'serial_number', )
 	
 	def Instrument_type(self, obj):
 		return obj.FK_instr_type
