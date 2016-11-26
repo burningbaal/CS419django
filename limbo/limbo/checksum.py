c308f249852201dadb268ac8eefbec0ad9bb4400
Keith McKinley <kmckinley@adaptivebiotech.com>
2016-11-26 09:35:26 -0800

    fixing json rendering of instr serializer

--- limbo/limbo/checksum.py
+++ limbo/limbo/checksum.py
@@ -4,7 +4,7 @@ from limbo.serializers import InstrumentSerializer
 
 def setChecksum(instrumentObj):
 	serial = InstrumentSerializer(instrumentObj)#, context={'request': request})
-	strInstrument = strInstrument + JSONRenderer().render(serial.data)
+	strInstrument = JSONRenderer().render(serial.data)
 	checksum = hashlib.sha1(strInstrument).hexdigest()
 	instrumentObj.checksum_string = checksum
 	instrumentObj.Instrument.save()
